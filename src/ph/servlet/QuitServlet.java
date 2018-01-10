d37d6358ddc9a1723b4becfad20d11c8c8cd70a5
hlzhangwhvcse <25538330@qq.com>
2018-01-10 16:21:01 +0800

    add some annotion

--- src/ph/servlet/QuitServlet.java
+++ src/ph/servlet/QuitServlet.java
@@ -16,7 +16,7 @@ public class QuitServlet extends HttpServlet
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        request.getSession(true).invalidate();
+        request.getSession(true).invalidate();//session.invalidate()是将session设置为失效，一般在退出时使用
 //		response.sendRedirect("index.jsp?para=退出成功");
         response.sendRedirect("index.jsp?para="+URLEncoder.encode("退出成功", "UTF-8"));
     }
