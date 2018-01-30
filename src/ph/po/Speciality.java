f4e1c7926fd89567459f7a00052e439d48ed8eac
hlzhangwhvcse <25538330@qq.com>
2018-01-30 14:51:13 +0800

    增加一个新专业

--- src/ph/po/Speciality.java
+++ src/ph/po/Speciality.java
@@ -4,6 +4,7 @@ public class Speciality
 {
     private int id;
     private String name;
+    private String description;
     public int getId() {
         return id;
     }
@@ -15,5 +16,11 @@ public class Speciality
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public String getDesc() {
+        return description;
+    }
+    public void setDesc(String desc) {
+        this.description = desc;
     }
 }
