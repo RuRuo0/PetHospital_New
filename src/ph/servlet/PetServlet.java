befad96c2456db47d103e0fb3ed499b7db5b62d0
hlzhangwhvcse <25538330@qq.com>
2018-01-30 18:05:02 +0800

    增加查询宠物的处理逻辑

--- src/ph/servlet/PetServlet.java
+++ src/ph/servlet/PetServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import java.util.List;
 import ph.dao.PetDAO;
 import ph.po.Pet;
 
@@ -32,6 +33,33 @@ public class PetServlet extends HttpServlet
         {
             delete(request, response);
         }
+
+    }
+
+    //查询宠物的处理逻辑，by hlzhang 20180130
+    private void searchPet(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException
+    {
+        String petName = request.getParameter("petName");
+        String ownerName = request.getParameter("ownerName");
+        try
+        {
+            List<Pet> pets = new PetDAO().search(petName, ownerName);
+            if(0==pets.size())
+            {
+                request.setAttribute("msg", "没有查到宠物信息");
+                request.getRequestDispatcher("/petSearch.jsp").forward(request, response);
+            }
+            else
+            {
+                request.setAttribute("pets", pets);
+                request.getRequestDispatcher("/petSearchResult.jsp").forward(request, response);
+            }
+        }
+        catch (Exception e)
+        {
+            request.setAttribute("msg", e.getMessage());
+            request.getRequestDispatcher("/petSearch.jsp").forward(request, response);
+        }
     }
 
     private void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -49,7 +77,21 @@ public class PetServlet extends HttpServlet
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        //add by hlzhang, 20180122
+        //doPost方法会被多个表单调用，查询宠物、增加宠物,因此这里需要根据不同表单传递的标示参数调用不同的方法
+        String m = request.getParameter("m");
+        if("search".equals(m))
+        {
+            searchPet(request, response);
+        }
+        else if("addPet".equals(m))
+        {
+            addPet(request, response);
+        }
+    }
+
+    //增加宠物的处理逻辑，by hlzhang 20180130
+    private void addPet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
         String name = request.getParameter("name");
         if("".equals(name))//宠物姓名不能为空
         {
@@ -93,7 +135,6 @@ public class PetServlet extends HttpServlet
             e.printStackTrace();
         }
     }
-
     private String getRuntimePath()
     {
         String path = "";
