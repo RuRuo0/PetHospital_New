8164e6df32a9e7d20517d639e31d0da026acf911
hlzhangwhvcse <25538330@qq.com>
2018-01-31 19:43:56 +0800

    增加了删除医生的delete()函数

--- src/ph/dao/VetDAO.java
+++ src/ph/dao/VetDAO.java
@@ -177,5 +177,40 @@ public class VetDAO
         }
     }
 
+    public void delete(int vetId) throws Exception
+    {
+        Connection con = null;
+        PreparedStatement ps = null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_ph","root","root");//  协议://域名(ip):端口/资源（数据库名）
+            ps = con.prepareStatement("delete from t_vet where id=?");
+            ps.setInt(1, vetId);
+            ps.executeUpdate();
+        }
+        catch(ClassNotFoundException e)
+        {
+            e.printStackTrace();
+            throw new Exception("找不到驱动:"+e.getMessage());//异常不能在底层丢失了
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            throw new Exception("数据库操作错误:"+e.getMessage());
+        }
+        finally
+        {
+            if(ps!=null)
+            {
+                ps.close();
+            }
+            if(con!=null)
+            {
+                con.close();
+            }
+        }
+    }
+
 
 }
